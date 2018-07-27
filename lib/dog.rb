46fce7698b29697f60594ac65cd18ea93f33db6a
art123craft <cammiebluecammie@gmail.com>
2018-07-27 20:24:59 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,12 +1,12 @@
-# dog.rb
-class Dog
-  def dog
-    puts "#{fido},#{snoppy}, #{lassie}."
-  end
-end
- fido = Dog.new
- snoopy = Dog.new
- lassie = Dog.new
+# # dog.rb
+# class Dog
+#   def dog
+#     puts "#{fido},#{snoppy}, #{lassie}."
+#   end
+# end
+# fido = Dog.new
+# snoopy = Dog.new
+# lassie = Dog.new
  
  
  
